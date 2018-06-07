138f33b14fedb87dc42eeefab9348984b8a10459
Andrew Wickham <andrew@awickham.com>
2018-06-07 12:30:49 -0400

    Trying to fix module caching

--- appveyor.setup.ps1
+++ appveyor.setup.ps1
@@ -5,13 +5,15 @@ $requiredModules = @(
 )
 
 foreach ($requiredModule in $requiredModules) {
-    $module = Get-Module -Name $requiredModule
+    $module = Get-Module -Name $requiredModule -ListAvailable
     if (-not $module) {
         Write-Host -Object "Install $requiredModule" -ForegroundColor DarkGreen
-        Install-Module -Name $requiredModule -Repository PSGallery | Out-Null
+        Install-Module -Name $requiredModule -Repository PSGallery -Force | Out-Null
     } else {
         Write-Host -Object "$requiredModule is cached" -ForegroundColor DarkGreen
+        Import-Module -Name $requiredModule -Force
     }
 }
 
+Write-Host -Object "Set StrictMode to 3.0" -ForegroundColor DarkGreen
 Set-StrictMode -Version 3.0
