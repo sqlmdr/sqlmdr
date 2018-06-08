6bf27551b7c28fdb0e99b014c9b633674aae5163
Andrew Wickham <andrew@awickham.com>
2018-06-08 10:02:49 -0400

    More VSTS

--- vsts.setup.ps1
+++ vsts.setup.ps1
@@ -9,7 +9,7 @@ foreach ($requiredModule in $requiredModules) {
     $module = Get-Module -Name $requiredModule -ListAvailable
     if (-not $module) {
         Write-Host -Object "Install $requiredModule" -ForegroundColor DarkGreen
-        Install-Module -Name $requiredModule -Repository PSGallery -Scope CurrentUser -SkipPublisherCheck -Force | Out-Null
+        Install-Module -Name $requiredModule -Repository PSGallery -Scope CurrentUser -Force | Out-Null
     } else {
         Write-Host -Object "$requiredModule is cached" -ForegroundColor DarkGreen
         Import-Module -Name $requiredModule -Force
@@ -18,10 +18,8 @@ foreach ($requiredModule in $requiredModules) {
 
 # Add current folder to PSModulePath
 try {
-    Write-Output "Adding local folder to PSModulePath"
+    Write-Output "Add local folder to PSModulePath"
     $ENV:PSModulePath = $ENV:PSModulePath + ";$pwd"
-    Write-Output "Added local folder to PSModulePath"
-    $ENV:PSModulePath.Split(';')
 }
 catch {
     Write-Error "Failed to add $pwd to PSModulePath - $_"
