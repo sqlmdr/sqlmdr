da65451ff1479c319198032318b9aa6c7f45410b
Andrew Wickham <andrew@awickham.com>
2018-06-12 07:43:28 -0400

    More codecov

--- Tests/appveyor.pester.ps1
+++ Tests/appveyor.pester.ps1
@@ -1,13 +1,79 @@
+<#
+
+.SYNOPSIS
+This script will invoke Pester tests, then serialize XML results and pull them in appveyor.yml
+
+.PARAMETER Finalize
+If Finalize is specified, we collect XML output, upload tests, and indicate build errors
+
+.PARAMETER IncludeCoverage
+Calculates coverage and sends it to codecov.io
+
+#>
 param(
-    [switch] $CodeCoverage,
-    [switch] $Finalize
+    [switch] $Finalize,
+    [switch] $IncludeCoverage
 )
 
 #Initialize some variables, move to the project root
 $PSVersion = $PSVersionTable.PSVersion.Major
 $TestFile = "TestResultsPS$PSVersion.xml"
 $ProjectRoot = $ENV:APPVEYOR_BUILD_FOLDER
+$ModuleBase = "$ProjectRoot\SQLMDR"
 Set-Location $ProjectRoot
+
+function Get-CodecovReport($Results, $ModuleBase) {
+    #handle coverage https://docs.codecov.io/reference#upload
+    $report = @{'coverage' = @{}}
+    #needs correct casing to do the replace
+    $ModuleBase = (Resolve-Path $ModuleBase).Path
+    # things we wanna a report for (and later backfill if not tested)
+    $allfiles = Get-ChildItem -File -Path "$ModuleBase\internal\functions", "$ModuleBase\functions" -Filter '*.ps1'
+
+    $missed = $results.CodeCoverage | Select-Object -ExpandProperty MissedCommands | Sort-Object -Property File, Line -Unique
+    $hits = $results.CodeCoverage | Select-Object -ExpandProperty HitCommands | Sort-Object -Property File, Line -Unique
+    $LineCount = @{}
+    $hits | ForEach-Object {
+        $filename = $_.File.Replace("$ModuleBase\", '').Replace('\', '/')
+        if ($filename -notin $report['coverage'].Keys) {
+            $report['coverage'][$filename] = @{}
+            $LineCount[$filename] = (Get-Content $_.File -Raw | Measure-Object -Line).Lines
+        }
+        $report['coverage'][$filename][$_.Line] = 1
+    }
+
+    $missed | ForEach-Object {
+        $filename = $_.File.Replace("$ModuleBase\", '').Replace('\', '/')
+        if ($filename -notin $report['coverage'].Keys) {
+            $report['coverage'][$filename] = @{}
+            $LineCount[$filename] = (Get-Content $_.File | Measure-Object -Line).Lines
+        }
+        if ($_.Line -notin $report['coverage'][$filename].Keys) {
+            #miss only if not already covered
+            $report['coverage'][$filename][$_.Line] = 0
+        }
+    }
+
+    $newreport = @{'coverage' = [ordered]@{}}
+    foreach ($fname in $report['coverage'].Keys) {
+        $Linecoverage = [ordered]@{}
+        for ($i = 1; $i -le $LineCount[$fname]; $i++) {
+            if ($i -in $report['coverage'][$fname].Keys) {
+                $Linecoverage["$i"] = $report['coverage'][$fname][$i]
+            }
+        }
+        $newreport['coverage'][$fname] = $Linecoverage
+    }
+
+    #backfill it
+    foreach ($target in $allfiles) {
+        $target_relative = $target.FullName.Replace("$ModuleBase\", '').Replace('\', '/')
+        if ($target_relative -notin $newreport['coverage'].Keys) {
+            $newreport['coverage'][$target_relative] = @{"1" = $null}
+        }
+    }
+    $newreport
+}
 
 # install required modules
 $requiredModules = @(
@@ -41,12 +107,11 @@ if(-not $Finalize)
         PassThru = $true
     }
 
-    if ($CodeCoverage.IsPresent) {
-        $codeCoveragePaths = @(
-            "$ProjectRoot\SQLMDR"
-        )
+    if ($IncludeCoverage.IsPresent) {
+        $codeCoveragePaths = Get-ChildItem $ModuleBase -File -Recurse -Include '*.ps1'
 
         $pesterParameters['CodeCoverage'] = $codeCoveragePaths
+        $pesterParameters['CodeCoverageOutputFile'] = "$ProjectRoot\PesterCoverage$PSVersion.xml"
     }
 
     Invoke-Pester @pesterParameters |
@@ -100,5 +165,10 @@ else
                 Format-List
 
             throw "$FailedCount tests failed."
+        }
+
+        if ($IncludeCoverage) {
+            $codecovReport = Get-CodecovReport -Results $results -ModuleBase $ModuleBase
+            $codecovReport | ConvertTo-Json -Depth 4 -Compress | Out-File -FilePath "$ProjectRoot\PesterResultsCoverage.json" -Encoding 'utf8'
         }
 }
